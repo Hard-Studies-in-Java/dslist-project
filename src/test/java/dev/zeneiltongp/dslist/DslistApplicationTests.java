6361b82e3aab977cf172fb8a1e4fe4072f07890b
zeneiltongpdev <zeneiltongranja@gmail.com>
2024-11-12 22:41:53 -0300

    sent new files

---
+++ src/test/java/dev/zeneiltongp/dslist/DslistApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.zeneiltongp.dslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
