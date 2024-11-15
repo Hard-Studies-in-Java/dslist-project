162f8b1894d8266ff804c091092b12133ba29563
zeneiltongpdev <zeneiltongranja@gmail.com>
2024-11-15 01:57:04 -0300

    [create] import.sql file & [updated] game.java score attribute

--- src/main/java/dev/zeneiltongp/dslist/entities/Game.java
+++ src/main/java/dev/zeneiltongp/dslist/entities/Game.java
@@ -9,30 +9,39 @@ import java.util.Objects;
 public class Game {
 
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+
 	private String title;
 
 	@Column(name = "game_year")
 	private Integer year;
+
 	private String genre;
-	private String platform;
+	private String platforms;
 	private String imgUrl;
+
+	@Column(columnDefinition = "TEXT")
 	private String shortDescription;
+
+	@Column(columnDefinition = "TEXT")
 	private String longDescription;
+
+	private Double score;
 
 	public Game() {
 	}
 
-	public Game(Long id, String title, Integer year, String genre, String platform, String imgUrl, String shortDescription, String longDescription) {
+	public Game(Long id, String title, Integer year, String genre, String platforms, String imgUrl, String shortDescription, String longDescription, Double score) {
 		this.id = id;
 		this.title = title;
 		this.year = year;
 		this.genre = genre;
-		this.platform = platform;
+		this.platforms = platforms;
 		this.imgUrl = imgUrl;
 		this.shortDescription = shortDescription;
 		this.longDescription = longDescription;
+		this.score = score;
 	}
 
 	public Long getId() {
@@ -67,12 +76,12 @@ public class Game {
 		this.genre = genre;
 	}
 
-	public String getPlatform() {
-		return platform;
+	public String getPlatforms() {
+		return platforms;
 	}
 
-	public void setPlatform(String platform) {
-		this.platform = platform;
+	public void setPlatforms(String platforms) {
+		this.platforms = platforms;
 	}
 
 	public String getImgUrl() {
@@ -97,6 +106,14 @@ public class Game {
 
 	public void setLongDescription(String longDescription) {
 		this.longDescription = longDescription;
+	}
+
+	public Double getScore() {
+		return score;
+	}
+
+	public void setScore(Double score) {
+		this.score = score;
 	}
 
 	@Override
